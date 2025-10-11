4de3133275957bb624206616a782fe5e584bcbe4
Fuad Daoud <fuad.daoud@icloud.com>
2025-10-11 14:56:34 +0300

    feat: add integration for prayer times and printing table

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -8,7 +9,7 @@ import (
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
 )
 
 const MAIN_COLOR = "#FAFAFA"
@@ -27,25 +28,72 @@ func getPublicIP() (string, error) {
 
 	return strings.TrimSpace(string(ip)), nil
 }
-func getPrayerInfo(ip string) (string, error) {
+
+type PrayerInfo struct {
+	Results struct {
+		Fajr    string `json:"Fajr"`
+		Duha    string `json:"Duha"`
+		Dhuhr   string `json:"Dhuhr"`
+		Asr     string `json:"Asr"`
+		Maghrib string `json:"Maghrib"`
+		Isha    string `json:"Isha"`
+	} `json:"results"`
+	Settings struct {
+		Name     string `json:"name"`
+		Location struct {
+			City    string `json:"city"`
+			State   string `json:"state"`
+			Country string `json:"country"`
+		} `json:"location"`
+		Latitude     string `json:"latitude"`
+		Longitude    string `json:"longitude"`
+		Timezone     string `json:"timezone"`
+		Method       int    `json:"method"`
+		Juristic     int    `json:"juristic"`
+		HighLatitude int    `json:"high_latitude"`
+		FajirRule    struct {
+			Type  int `json:"type"`
+			Value int `json:"value"`
+		} `json:"fajir_rule"`
+		MaghribRule struct {
+			Type  int `json:"type"`
+			Value int `json:"value"`
+		} `json:"maghrib_rule"`
+		IshaRule struct {
+			Type  int `json:"type"`
+			Value int `json:"value"`
+		} `json:"isha_rule"`
+		TimeFormat int `json:"time_format"`
+	} `json:"settings"`
+	Success bool `json:"success"`
+}
+
+func getPrayerInfo(ip string) (PrayerInfo, error) {
 	resp, err := http.Get("https://islamicfinder.us/index.php/api/prayer_times?user_ip=" + ip)
 	if err != nil {
-		return "", err
+		return PrayerInfo{}, err
 	}
 	defer resp.Body.Close()
 
-	info, err := io.ReadAll(resp.Body)
+	jsonString, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", nil
+		return PrayerInfo{}, nil
 	}
-	return string(info), nil
+	var result PrayerInfo
+	err = json.Unmarshal(jsonString, &result)
+	if err != nil {
+		fmt.Printf("Could not unmarshal the response: %v\n", err)
+		fmt.Printf("jsonString: %v\n", jsonString)
+		os.Exit(1)
+	}
+	return result, nil
 }
 
 type model struct {
 	width  int
 	height int
 	ip     string
-	info   string
+	info   PrayerInfo
 }
 
 func InitialModel() model {
@@ -82,23 +130,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	content := lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color(MAIN_COLOR)).
-		BorderStyle(lipgloss.RoundedBorder()).
-		BorderForeground(lipgloss.Color(MAIN_COLOR)).
-		Padding(1).
-		Width(50).
-		Align(lipgloss.Center).
-		Render(m.info)
+	t := table.New()
+	t.Headers("Prayer", "time")
+	t.Row("Fajr", m.info.Results.Fajr)
+	t.Row("Dhuhr", m.info.Results.Dhuhr)
+	t.Row("Asr", m.info.Results.Asr)
+	t.Row("Maghrib", m.info.Results.Maghrib)
+	t.Row("Isha", m.info.Results.Isha)
+	return t.Render() + "\n"
 
-	return lipgloss.Place(
-		m.width,
-		m.height,
-		lipgloss.Center,
-		lipgloss.Center,
-		content,
-	)
+	// return lipgloss.Place(
+	// 	m.width,
+	// 	m.height,
+	// 	lipgloss.Center,
+	// 	lipgloss.Center,
+	// 	content,
+	// )
 }
 
 func main() {
